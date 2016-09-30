220a1f535c7e8e5689ab7306370e64fdcb0b64c8
Patrick Cullen <patrick.cullen@washpost.com>
2016-09-30 11:09:20 -0400

    simplified script

--- cloud-compose/templates/system.raid.sh
+++ cloud-compose/templates/system.raid.sh
@@ -4,12 +4,8 @@ yum -y install mdadm
 
 {%- for raid in aws.raid %}
 
-mdadm --create --verbose {{ raid.block }} --level={{ raid.level }} --chunk={{ raid.chunk|default("1024", true) }} --name={{ raid.name }} --raid-devices={{ raid.devices|length }} {{ raid.devices|join(' ') }}
-{%- if raid.journal is defined and not raid.journal %}
-mkfs -t {{ raid.file_system }} -O ^has_journal {{ raid.block }}
-{%- else %}
-mkfs -t {{ raid.file_system }} {{ raid.block }}
-{%- endif %}
+mdadm --create --verbose {{ raid.block }} --level={{ raid.level }} --chunk={{ raid.chunk|default("64", true) }} --name={{ raid.name }} --raid-devices={{ raid.devices|length }} {{ raid.devices|join(' ') }}
+mkfs.{{ raid.file_system }} {{ raid.mkfs_options|default("", true) }} {{ raid.block }}
 echo -e '{{ raid.block }}\t{{ raid.mount }}\t{{ raid.file_system }}\t{{ raid.options|default("defaults,noatime", true) }}\t0\t0' >> /etc/fstab
 mkdir -p {{ raid.mount }} 
 mount {{ raid.mount }}
