d1f9a42c8426bd15730eee552b27779e44e93317
Patrick Cullen <patrick.cullen@washpost.com>
2016-09-29 21:51:20 -0400

    updated raid script

--- cloud-compose/templates/system.raid.sh
+++ cloud-compose/templates/system.raid.sh
@@ -5,8 +5,12 @@ yum -y install mdadm
 {%- for raid in aws.raid %}
 
 mdadm --create --verbose {{ raid.block }} --level={{ raid.level }} --name={{ raid.name }} --raid-devices={{ raid.devices|length }} {{ raid.devices|join(' ') }}
+{%- if raid.journal is defined and not raid.journal %}
+mkfs -t {{ raid.file_system }} -O ^has_journal {{ raid.block }}
+{%- else %}
 mkfs -t {{ raid.file_system }} {{ raid.block }}
-echo -e '{{ raid.block }}\t{{ raid.mount }}\t{{ raid.file_system }}\t{{ raid.options|default("defaults", true) }}\t0\t0' >> /etc/fstab
+{%- endif %}
+echo -e '{{ raid.block }}\t{{ raid.mount }}\t{{ raid.file_system }}\t{{ raid.options|default("defaults,noatime", true) }}\t0\t0' >> /etc/fstab
 mkdir -p {{ raid.mount }} 
 mount {{ raid.mount }}
 
