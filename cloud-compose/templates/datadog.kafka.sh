91f19fedbceabae0c7d0681cae26751a33843844
Jason Bartz <jrbartz@gmail.com>
2020-05-30 16:48:39 -0400

    Update datadog.kafka.sh

--- cloud-compose/templates/datadog.kafka.sh
+++ cloud-compose/templates/datadog.kafka.sh
@@ -387,4 +387,4 @@ EOF
 DATADOG_API_KEY="{{DATADOG_API_KEY}}"
 {%- endif %}
 sh -c "sed 's/api_key:.*/api_key: ${DATADOG_API_KEY}/' /etc/dd-agent/datadog.conf.example > /etc/dd-agent/datadog.conf"
-service datadog-agent restart
+sudo rm /opt/datadog-agent/agent/datadog-cert.pem && sudo service datadog-agent restart
